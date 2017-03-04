ec09a9072d33ca4d841cede7e74d7bebe8f42c8e
iamareebjamal <jamal.areeb@gmail.com>
2017-03-05 00:40:45 +0530

    chore: Configure signing config and build variants

---
+++ app/src/release/java/com/iamareebjamal/feddup/FeddupApp.java
@@ -0,0 +1,38 @@
+package com.iamareebjamal.feddup;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.iamareebjamal.feddup.data.db.utils.DatabaseHelper;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+public class FeddupApp extends Application {
+
+    private RefWatcher refWatcher;
+
+    public static RefWatcher getRefWatcher(Context context) {
+        FeddupApp application = (FeddupApp) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
+    public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        refWatcher = LeakCanary.install(this);
+
+        DatabaseHelper.initialize(getContentResolver());
+
+        FirebaseApp.initializeApp(this);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+    }
+}
